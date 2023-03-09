b380c415386d4bf99f5d943d753842f959e2358f
leon.smc <123242466+leonsmc@users.noreply.github.com>
2023-03-09 15:31:58 +0100

    Restored the Commit without previous changfes

--- main.py
+++ main.py
@@ -1,61 +1,62 @@
 import turtle as t
-import math
 
 t.speed(0)
 t.hideturtle()
 t.penup()
 t.setpos(-200, -200)
 dot_size = 25
-abfrageposition = 0
+current_pos = 0
 
 def playfield_init():
-    global reckteck
+    global rect
     with open("feld.txt") as file:
         lines = file.readlines()
-        reckteck = [[int(c) for c in line.strip()] for line in lines]
+        rect = [[int(c) for c in line.strip()] for line in lines]
 
 def playfield_swap():
-    global reckteck
+    global rect
     with open("feld.txt", "w") as file:
-        for line in reckteck:
+        for line in rect:
             file.write("".join(str(c) for c in line) + "\n")
 
-def count_living(rechteck):
-    global abfrageposition, count
-    y_achse = len(rechteck)
-    x_achse = len(rechteck[0])
-    i, j = divmod(abfrageposition, x_achse)
+def count_living(rect):  #zählt die überlebenden Nachbarn
+    global current_pos, count
+    num_rows = len(rect)
+    num_cols = len(rect[0])
+    i, j = divmod(current_pos, num_cols)
     count = 0
     for di in [-1, 0, 1]:
         for dj in [-1, 0, 1]:
             if di == 0 and dj == 0:
                 continue
-            ni = (i + di) % y_achse
-            nj = (j + dj) % x_achse
-            if rechteck[ni][nj] == 1:
+            ni = (i + di) % num_rows
+            nj = (j + dj) % num_cols
+            if rect[ni][nj] == 1:
                 count += 1
-    abfrageposition += 1
-    return abfrageposition, rechteck
+    current_pos += 1
+    return current_pos, rect
 
-def check_rules(rechteck,abfrageposition , count):
-    i, j = divmod(abfrageposition, len(rechteck[0]))
+def check_rules(rect,current_pos , count):
+    i, j = divmod(current_pos, len(rect[0]))
     try:
-        if rechteck[i][j] == 1 and (count < 2 or count > 3):
-            rechteck[i][j] = 0
-        elif rechteck[i][j] == 0 and count == 3:
-            rechteck[i][j] = 1
+        if rect[i][j] == 1 and (count < 2 or count > 3):
+            rect[i][j] = 0
+        elif rect[i][j] == 0 and count == 3:
+            rect[i][j] = 1
     except:
         ()
-def playfield_draw(dateipfad):
-    with open(dateipfad) as f:
+def playfield_draw(file_path):
+    with open(file_path) as f:
         for i, line in enumerate(f):
             for j, char in enumerate(line.strip()):
                 try:
                     t.penup()
-                    t.goto(j * 30, -i * 30)
+                    t.goto(j * 30, -i * 30) # multiply by 30 to account for dot size
+                    #if char == "0":
+                        #t.dot(25, "white")
                     if char == "1":
                         t.dot(25, "black")
-                    t.forward(5)
+                    t.forward(5) # add a gap of 5 between dots
                 except:
                     exit()
 def main():
@@ -63,20 +64,17 @@ def main():
     while True:
         playfield_draw("feld.txt")
         while True:
-            count_living(reckteck)
-            check_rules(reckteck, abfrageposition, count)
+            count_living(rect)
+            check_rules(rect, current_pos, count)
             playfield_swap()
             playfield_draw("feld.txt")
             t.clear()
-            for _ in range(abfrageposition):
-                check_rules(reckteck, abfrageposition, count)
+            for _ in range(current_pos):
+                check_rules(rect, current_pos, count)
                 playfield_swap()
             t.update()
 
 if __name__ == "__main__":
     main()
 
-print(abfrageposition) 
-
-#Fehler zu beheben: Es wird immer nur die eine Position pro durchlauf verändert und dann geprintet
-#                   und dann erst beim nächsten durchlauf die nächste position
+print(current_pos)
