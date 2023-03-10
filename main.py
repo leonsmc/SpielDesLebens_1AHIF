31950344963b320a83034d8b5d721cb5f1c553bd
leon.smc <123242466+leonsmc@users.noreply.github.com>
2023-03-10 13:32:48 +0100

    fixed to code
    the starting value wont change every tick and global variables are disabled and the 2 playfield variant is now activ

--- main.py
+++ main.py
@@ -4,26 +4,16 @@ t.speed(0)
 t.hideturtle()
 t.penup()
 t.setpos(-200, -200)
-dot_size = 25
-current_pos = 0
 
 def playfield_init():
-    global rect
     with open("feld.txt") as file:
         lines = file.readlines()
-        rect = [[int(c) for c in line.strip()] for line in lines]
+        playfield1 = [[int(c) for c in line.strip()] for line in lines]
+    return playfield1
 
-def playfield_swap():
-    global rect
-    with open("feld.txt", "w") as file:
-        for line in rect:
-            file.write("".join(str(c) for c in line) + "\n")
-
-def count_living(rect):  #zählt die überlebenden Nachbarn
-    global current_pos, count
-    num_rows = len(rect)
-    num_cols = len(rect[0])
-    i, j = divmod(current_pos, num_cols)
+def count_living(playfield, i, j):
+    num_rows = len(playfield)
+    num_cols = len(playfield[0])
     count = 0
     for di in [-1, 0, 1]:
         for dj in [-1, 0, 1]:
@@ -31,50 +21,54 @@ def count_living(rect):  #zählt die überlebenden Nachbarn
                 continue
             ni = (i + di) % num_rows
             nj = (j + dj) % num_cols
-            if rect[ni][nj] == 1:
+            if playfield[ni][nj] == 1:
                 count += 1
-    current_pos += 1
-    return current_pos, rect
+    return count
 
-def check_rules(rect,current_pos , count):
-    i, j = divmod(current_pos, len(rect[0]))
-    try:
-        if rect[i][j] == 1 and (count < 2 or count > 3):
-            rect[i][j] = 0
-        elif rect[i][j] == 0 and count == 3:
-            rect[i][j] = 1
-    except:
-        ()
-def playfield_draw(file_path):
-    with open(file_path) as f:
-        for i, line in enumerate(f):
-            for j, char in enumerate(line.strip()):
-                try:
-                    t.penup()
-                    t.goto(j * 30, -i * 30) # multiply by 30 to account for dot size
-                    #if char == "0":
-                        #t.dot(25, "white")
-                    if char == "1":
-                        t.dot(25, "black")
-                    t.forward(5) # add a gap of 5 between dots
-                except:
-                    exit()
+def check_rules(playfield1):
+    num_rows = len(playfield1)
+    num_cols = len(playfield1[0])
+    playfield2 = [[0 for j in range(num_cols)] for i in range(num_rows)]
+    for i in range(num_rows):
+        for j in range(num_cols):
+            count = count_living(playfield1, i, j)
+            if playfield1[i][j] == 1 and (count < 2 or count > 3):
+                playfield2[i][j] = 0
+            elif playfield1[i][j] == 0 and count == 3:
+                playfield2[i][j] = 1
+            else:
+                playfield2[i][j] = playfield1[i][j]
+    return playfield2
+
+def playfield_swap(playfield1, playfield2):
+    for i in range(len(playfield2)):
+        for j in range(len(playfield2[0])):
+            playfield1[i][j] = playfield2[i][j]
+
+                
+def playfield_draw(playfield):
+    for i in range(len(playfield)):
+        for j in range(len(playfield[0])):
+            try:
+                t.penup()
+                t.goto(j * 30, -i * 30)
+
+                if playfield[i][j] == 1:
+                    t.dot(25, "black")
+                t.forward(5)
+                
+            except:
+                exit()
+
 def main():
-    playfield_init()
+    playfield1 = playfield_init()
     while True:
-        playfield_draw("feld.txt")
-        while True:
-            count_living(rect)
-            check_rules(rect, current_pos, count)
-            playfield_swap()
-            playfield_draw("feld.txt")
-            t.clear()
-            for _ in range(current_pos):
-                check_rules(rect, current_pos, count)
-                playfield_swap()
-            t.update()
+        playfield2 = check_rules(playfield1)
+        playfield_swap(playfield1, playfield2)
+        playfield_draw(playfield1)
+        playfield1 = playfield2
+        t.clear()
+        t.update()
 
 if __name__ == "__main__":
     main()
-
-print(current_pos)
